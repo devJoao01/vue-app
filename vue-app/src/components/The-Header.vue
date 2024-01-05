946774dbac137e3f9f8900f157bdac50377ef0ab
João Vitor <jvlima0002020@gmail.com>
2024-01-05 15:35:38 -0300

    new structure for project

---
+++ vue-app/src/components/The-Header.vue
@@ -0,0 +1,13 @@
+<template>
+    HEADER asdjasldkjas alskdjaslkd
+</template>
+
+<script>
+export default {
+    
+}
+
+</script>
+<style scoped>
+    @import '../sass/app.scss';
+</style>
---
+++ vue-app/src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+  <Header />
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+// Components
+import Header from '../components/The-Header';
+
+export default defineComponent({
+  name: 'HomeView',
+
+  components: {
+    Header,
+  },
+});
+</script>
